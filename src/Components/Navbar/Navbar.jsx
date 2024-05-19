8ec846fe94aaaa97d66a1e23fee50483ea0a185b
AbishekJUNGthakuri19 <abishekjung00@gmail.com>
2024-05-19 19:21:28 +0545

    images

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './Navbar.css'
+import logo from '../../assets/logo.png'
 
 export const Navbar = () => {
   return (
     <div className='navbar' >
-        Navbar
+       
     </div>
   )
 }
