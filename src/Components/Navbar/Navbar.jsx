d336d75ef30e0d360270572cc5daf1468ca20cf1
AbishekJUNGthakuri19 <abishekjung00@gmail.com>
2024-05-21 09:40:02 +0545

    responsive

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,9 +2,11 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-scroll';
 import './Navbar.css';
 import logo from '../../assets/logo.png';
+import menu_icon from '../../assets/menu-icon.png';
 
 export const Navbar = () => {
   const [sticky, setSticky] = useState(false);
+  const [menu, setMenu] = useState(false);
 
   useEffect(() => {
     window.addEventListener('scroll', () => {
@@ -12,10 +14,14 @@ export const Navbar = () => {
     });
   }, []);
 
+  const toggleMenu = ()=>{
+         menu ? setMenu(false) : setMenu(true);
+  }
+
   return (
     <nav className={`container ${sticky ? 'dark-nav' : ''} `}>
       <img src={logo} alt="" className="logo" />
-      <ul>
+      <ul className={menu ? '' : 'hide-menu'} >
         <li><Link to="hero" offset ={0}   smooth={true} duration={500}>Home </Link></li>
         <li><Link to="programs" offset ={-260}   smooth={true} duration={500}>Program</Link></li>
         <li><Link to="about" offset ={-130}   smooth={true} duration={500}>About Us</Link></li>
@@ -23,6 +29,7 @@ export const Navbar = () => {
         <li><Link to="testimonial" offset ={-260}   smooth={true} duration={500}>Testimonial</Link></li>
         <li> <button className='btn'>Contact Us</button> </li> 
       </ul>
+      <img src={menu_icon} alt="" className='menu-icon' onClick={toggleMenu} />
     </nav>
   );
 };
